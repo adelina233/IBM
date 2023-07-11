6d203b74dc17207c63d95df055d7df2e7a311d63
Novac Adelina <121970647+adelina233@users.noreply.github.com>
2023-07-11 13:34:31 +0300

    Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,19 +3,28 @@ package org.example;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.functions;
 
 public class Main {
     public static void main(String[] args) {
-        SparkSession sparkSession = SparkSession.builder()
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("Java Spark SQL")
                 .master("local")
-                .appName("Read_CSV")
+                .config("spark.some.config.option", "some-value")
                 .getOrCreate();
 
-        Dataset<Row> dataset = sparkSession.read()
-                .format("csv")
-                .option("header", "true")
-                .load("C:/Users/40759/IdeaProjects/untitled10/src/main/resources/erasmus.csv");
+        Dataset<Row> dataset = spark.read().option("header", true).csv("C:/Users/40759/IdeaProjects/untitled10/src/main/resources/erasmus.csv");
 
-        dataset.show();
+        dataset.printSchema();
+        dataset.select("Mobility Duration").show();
+        dataset.select(functions.lower(functions.col("Sending Country Code")), functions.col("Participant Age")).show();
+        dataset.filter(functions.col("Participant Age").$greater(25)).show();
+        dataset.groupBy("Participant Age").count().show();
+        dataset.groupBy("Receiving Country Code", "Sending Country Code")
+                .count()
+                .withColumnRenamed("count", "Number Of Students")
+                .orderBy(functions.col("Receiving Country Code").desc())
+                .show();
     }
 }
