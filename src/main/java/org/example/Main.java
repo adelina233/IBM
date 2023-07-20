8dddb8817f2696b09cca3015902aefa4f4ce5377
Novac Adelina <adelina.novac@student.upt.ro>
2023-07-20 13:55:24 +0300

    Tema1IBM

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.functions;
+import static org.apache.spark.sql.functions.col;
+import org.apache.spark.sql.SaveMode;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -17,14 +20,29 @@ public class Main {
         Dataset<Row> dataset = spark.read().option("header", true).csv("C:/Users/40759/IdeaProjects/untitled10/src/main/resources/erasmus.csv");
 
         dataset.printSchema();
-        dataset.select("Mobility Duration").show();
-        dataset.select(functions.lower(functions.col("Sending Country Code")), functions.col("Participant Age")).show();
-        dataset.filter(functions.col("Participant Age").$greater(25)).show();
-        dataset.groupBy("Participant Age").count().show();
+        dataset=dataset.filter(functions.col("Receiving Country Code").isin("PL", "FR","BE"));
         dataset.groupBy("Receiving Country Code", "Sending Country Code")
                 .count()
-                .withColumnRenamed("count", "Number Of Students")
                 .orderBy(functions.col("Receiving Country Code").desc())
-                .show();
+                .show(100);
+        saveData(dataset,"PL","Polonia");
+        saveData(dataset,"FR","Franta");
+        saveData(dataset,"ES","Belgia");
+    }
+
+    public static void saveData(Dataset<Row> dataset, String countryCode, String tableName) {
+        dataset
+                .filter(col("Receiving Country Code").isin(countryCode))
+                .groupBy("Receiving Country Code", "Sending Country Code")
+                .count().orderBy("Receiving Country Code", "Sending Country Code")
+                .write()
+                .mode(SaveMode.Overwrite)
+                .format("jdbc")
+                .option("driver", "com.mysql.cj.jdbc.Driver")
+                .option("url", "jdbc:mysql://127.0.0.1:3306/tema")
+                .option("dbtable", tableName)
+                .option("user", "root")
+                .option("password", "Ade1234-")
+                .save(tableName + ".tema");
     }
 }
